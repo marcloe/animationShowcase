908c9143da559931a0c04cb8fdff75e8232dc21e
marcloe <76460507+marcloe@users.noreply.github.com>
2023-07-21 16:14:58 +0200

    update 4:15

--- app.js
+++ app.js
@@ -1,46 +1,98 @@
-/* if (/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-  // When ready, auto-scroll 1px to hide URL bar
-  window.addEventListener("load", function () {
-      // Set a timeout...
-      setTimeout(function () {
-          // Hide the address bar!
-          window.scrollTo(0, 1);
-      }, 0);
-  });
-} */
-
 // Navigation Logic
 
 startPage = document.querySelectorAll(".wrapperhome").item(0);
 showCase = document.querySelectorAll(".wrapper").item(0);
+question = document.getElementById('question');
+questionCount = 0;
 
 function switchToShowcase() {
 
   if(getComputedStyle(showCase).visibility == "hidden" || "undefined") {
+    if(questionCount <=2) {
       showCase.style.visibility = "visible";
       startPage.style.visibility = "hidden";
-    }
-    else {
+      questionPeriod();
+    } else {
       showCase.style.visibility = "hidden";
       startPage.style.visibility = "visible"
+      questionCount = 0;
+      console.log("backhome")
+    }
   }
-  console.log("click!")
 }
 
 document.getElementById('startButton').addEventListener('click', switchToShowcase)
 
-//QuestionChanger
-/* 
+//QuestionCounter
+
 function infuseQuestion() {
+  questionCount+=1
+  console.log(questionCount);
   if(questionCount == 1) {
-    questionBoard.innerHTML = "Do you feel in control of the situation"
+    question.textContent = "How do you feel today?";
+    console.log("infused!");
   }
   if(questionCount == 2) {
-    questionBoard.innerHTML = "Do you feel in control of the situation"
+    question.textContent = "Is this a high-energy feeling?"
+    console.log("infused!");
   }
+  if (questionCount == 3) {
+    question.textContent = "Do you feel in control of the situation?"
+    console.log("infused!");
+  }
+
+
 }
 
- */
+
+//Timer Logic für drawingPeriod
+
+function questionPeriod() {
+  console.log("questioningStarted")
+  infuseQuestion();
+  question.style.visibility = "visible"; // Show the element
+  const seconds = 4; // Set the timer duration in seconds
+  let remainingSeconds = seconds;
+
+  const timerInterval = setInterval(() => {
+    remainingSeconds--;
+
+    if (remainingSeconds <= 0) {
+      clearInterval(timerInterval);
+      // Call the function to change visibility when the timer reaches zero
+      question.style.visibility = "hidden"; // Hide the element after 2 seconds (example)
+      drawingPeriod();
+    }
+  }, 1000); // 1000 milliseconds = 1 second
+}
+
+function drawingPeriod() {
+  console.log("drawingStarted")
+
+  const seconds = 10; // Set the timer duration in seconds
+  let remainingSeconds = seconds;
+  const loadingBar = document.getElementById("loadingBar");
+
+  const timerInterval = setInterval(() => {
+    remainingSeconds--;
+    const progress = (seconds - remainingSeconds) / seconds;
+    loadingBar.style.width = progress * 100 + "%";
+
+    if (remainingSeconds <= 0) {
+      clearInterval(timerInterval);
+      loadingBar.style.width = "0";
+      // Call the function to change visibility when the timer reaches zero
+
+
+      if(questionCount<=2) {
+        questionPeriod();
+      } else {
+        switchToShowcase();
+      }
+
+    }
+  }, 1000); // 1000 milliseconds = 1 second
+}
 
 //Animation Loader
 
